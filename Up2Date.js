3e0a9eaba6cb7a6934cc7d52a15200d2826c3baf
cupglassDEV <70825723+cupglassDEV@users.noreply.github.com>
2024-08-03 19:14:39 +0700

    Create Up2Date.js

---
+++ Up2Date.js
@@ -0,0 +1,24 @@
+//esm is good👌
+import { setFailed, setOutput as _ } from "@actions/core";
+import fs from "node:fs/promises";
+const aboutPath = "about.json"
+/** @type {(f:string)=>string[]} */
+function toIcsCollection(f){
+    return [
+        `${f}.ics`,
+        `${f}-detailed.ics`,
+    ]
+}
+async function main(){
+  const year = (new Date(new Date.now())).getUTCFullYear()
+  await fs.stat(aboutPath)
+  for (const each of toIcsCollection(aboutPath)) await fs.stat(each)
+  /** @type {string} */
+  const content = Buffer.from(await fs.readFile(aboutPath)).toString('utf8')
+  /** @type {object|string} */
+  let data = JSON.parse(content)
+  data['preferredEdition'] = data['preferredEdition']==year.toString()?((year+1).toString()):(year.toString())
+  data = JSON.stringify(data);
+  await fs.writeFile(path, data)
+}
+main().catch(e=>setFailed(e.message))
