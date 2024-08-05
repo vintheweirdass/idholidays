fac723f3475392aef53e3dfd2e44cc0d3191f639
cupglassDEV <70825723+cupglassDEV@users.noreply.github.com>
2024-08-05 07:58:12 +0700

    Update Up2Date.js

--- Up2Date.js
+++ Up2Date.js
@@ -10,14 +10,15 @@ function toIcsCollection(f){
     ]
 }
 async function main(){
-  const year = (new Date(Date.now())).getUTCFullYear()
+  process.TZ = "UTC0"
+  const year = (new Date(Date.now())).getFullYear()
   await fs.stat(aboutPath)
   for (const each of toIcsCollection(year.toString())) await fs.stat(each)
   /** @type {string} */
   const content = Buffer.from(await fs.readFile(aboutPath)).toString('utf8')
   /** @type {object|string} */
   let data = JSON.parse(content)
-  data['preferredEdition'] = data['preferredEdition']==year.toString()?((year+1).toString()):(year.toString())
+  data['preferredEdition'] = data['preferredEdition']==(year.toString())?((year+1).toString()):(year.toString())
   data = JSON.stringify(data);
   await fs.writeFile(aboutPath, data)
 }
