79275013337073832082929cb70efb2bc13af4a9
cupglassDEV <70825723+cupglassDEV@users.noreply.github.com>
2024-08-05 08:06:42 +0700

    Update Up2Date.js

--- Up2Date.js
+++ Up2Date.js
@@ -1,5 +1,5 @@
 //esm is good👌
-import { setFailed, setOutput } from "@actions/core";
+import { setFailed } from "@actions/core";
 import fs from "node:fs/promises";
 const aboutPath = "about.json"
 /** @type {(f:string)=>string[]} */
@@ -12,19 +12,19 @@ function toIcsCollection(f){
 async function main(){
   process.TZ = "UTC0"
   const year = (new Date(Date.now())).getFullYear()
-  setOutput(`getting file stats for ${aboutPath}`)
+  console.log(`getting file stats for ${aboutPath}`)
   await fs.stat(aboutPath)
-  setOutput(`getting file stats for current year`)
+  console.log(`getting file stats for current year`)
   for (const each of toIcsCollection(year.toString())) await fs.stat(each)
   /** @type {string} */
   const content = Buffer.from(await fs.readFile(aboutPath)).toString('utf8')
   /** @type {object|string} */
   let data = JSON.parse(content)
   let yearCalc = data['preferredEdition']==(year.toString())?((year+1).toString()):(year.toString())
-  setOutput(`setting year to ${yearCalc}`)
+  console.log(`setting year to ${yearCalc}`)
   data['preferredEdition'] = yearCalc
   data = JSON.stringify(data);
-  setOutput(`writing back to ${aboutPath}`)
+  console.log(`writing back to ${aboutPath}`)
   await fs.writeFile(aboutPath, data)
 }
 main().catch(e=>setFailed(e.message))
